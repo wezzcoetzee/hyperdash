15fa8a6666431a72b1082c1374e2e36e45ee2409
Wesley Coetzee <coetzeewr@gmail.com>
2026-07-23 20:11:58 +1200

    feat: shake detection

--- Hyperdash/Feedback/ShakeDetection.swift
+++ Hyperdash/Feedback/ShakeDetection.swift
@@ -4,15 +4,8 @@ extension Notification.Name {
     static let deviceDidShake = Notification.Name("app.deviceDidShake")
 }
 
-private final class ShakeDetectingViewController: UIViewController {
-    override var canBecomeFirstResponder: Bool { true }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        becomeFirstResponder()
-    }
-
-    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             NotificationCenter.default.post(name: .deviceDidShake, object: nil)
         }
@@ -20,14 +13,8 @@ private final class ShakeDetectingViewController: UIViewController {
     }
 }
 
-private struct ShakeDetector: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController { ShakeDetectingViewController() }
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-}
-
 extension View {
     func onShake(perform action: @escaping () -> Void) -> some View {
-        background(ShakeDetector().allowsHitTesting(false))
-            .onReceive(NotificationCenter.default.publisher(for: .deviceDidShake)) { _ in action() }
+        onReceive(NotificationCenter.default.publisher(for: .deviceDidShake)) { _ in action() }
     }
 }
